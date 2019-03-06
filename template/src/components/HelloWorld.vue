604fa37da96dcbe1ac4fb58ceadfe72b55cba592
huahaitao <huahaitao739@pingan.com.cn>
2019-03-06 20:59:03 +0800

    add template

---
+++ template/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hello {{ data }}!</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      data: 'World'
+    }
+  }
+}
+</script>
+
